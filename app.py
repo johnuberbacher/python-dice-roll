d8eaffef363e16bda9986cd5770aebabf8758509
John Uberbacher <johnuberbacher@johns-mbp.hnhad.holyname.org>
2017-08-22 10:35:57 -0400

    Updated with logic, removed MODE and Start

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
-MODE 80,25
-@echo off
+import random
 
-:start
+def die_roll():
+    return random.randint(1, 6)
+
+def calculate_roll():
+    total = 0
+    for turn in range(2):
+        total += die_roll()
+    return total
+print calculate_roll()
